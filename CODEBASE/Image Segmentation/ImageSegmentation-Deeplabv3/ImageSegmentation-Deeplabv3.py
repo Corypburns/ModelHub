9ba23784f96fc6e047fe8bc130c771130cce2b86
CoryBurns12 <burnscory2001@gmail.com>
2025-07-17 18:06:56 -0400

    Chunk commit

--- CODEBASE/Image Segmentation/ImageSegmentation-Deeplabv3/ImageSegmentation-Deeplabv3.py
+++
@@ -1,6 +0,0 @@
-import torch as t, os
-from torchvision import models
-from sklearn.model_selection import train_test_split as tts
-
-model = models.segmentation.deeplabv3_resnet50(pretrained=True)
-images = ''
